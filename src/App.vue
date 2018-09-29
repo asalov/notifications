59550df7410a5c8ac2725c2150a9ee3121c04a52
Aleksandar Salov <aleksandarsalov@yahoo.com>
2018-09-29 20:08:42 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Notifications />
+  </div>
+</template>
+
+<script>
+import Notifications from './components/Notifications';
+
+export default {
+  name: 'App',
+  components: {
+    Notifications,
+  },
+};
+</script>
+
+<style>
+body {
+  background: #eee;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+#app {
+  margin-top: 60px;
+}
+</style>
